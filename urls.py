4d88bff2586135fc52b742f6626cbddb11ac72f8
Ignacio Feijoo <ignacionf@gmail.com>
2009-07-02 18:46:32 -0300

    BASE_URL para el urls.py

--- urls.py
+++ urls.py
@@ -3,13 +3,9 @@ from django.contrib import admin
 from django.conf import settings
 admin.autodiscover()
 
-
-# vacio si para acceder a pysysinfo se hace desde la misma url
-BASE_URL='pysysinfo/'
-
 urlpatterns = patterns('',
-        (r'%sstatic/(?P<path>.*)$' % BASE_URL, 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-	(r'^%s$' % BASE_URL, include('home.urls')),
-	(r'^%shome/' % BASE_URL, include('home.urls')),
-	(r'^%s__admin/(.*)' % BASE_URL, admin.site.root),
+        (r'static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+	(r'^$', include('home.urls')),
+	(r'^home/', include('home.urls')),
+	(r'^__admin/(.*)', admin.site.root),
 )
